b68e7e7a5401d0e3ccde58e0f117b7eaa2c9631c
Gabor Varga <gabor@agora.hu>
2019-02-22 21:21:13 +0100

    Correctly implemented unary multiypl and addition operators.

--- include/ScalarUnaryOp.h
+++ include/ScalarUnaryOp.h
@@ -3,6 +3,7 @@
 
 
 #include "ScalarBase.h"
+#include <math.h>
 
 
 namespace diff
@@ -14,7 +15,7 @@ namespace diff
 
     public:
         using Partial = typename Op::Output;
-        using PartialSegment = decltype( std::declval< typename Expr::Partial >().segment( int(), int() ) );
+        using PartialSegment = decltype( std::declval< Partial >().segment( int(), int() ) );
         using Record = std::pair< ParameterPtr, PartialSegment >;
         using Iterator = Iterator< ScalarUnaryOp< Expr, Op > >;
 
@@ -22,7 +23,7 @@ namespace diff
         ScalarUnaryOp( const Expr& expr, const Op& op )
             : expr_( expr )
             , op_( op )
-            , partial_( op_.applyToPartial( expr_.partial() ) )
+            , partial_( op_.applyToPartial( expr_.value(), expr_.partial() ) )
         {
         }
 
@@ -73,6 +74,7 @@ namespace diff
 
         Partial partial_;
     };
+    
 
     template< typename Expr >
     class UnaryAdditionOp
@@ -92,9 +94,9 @@ namespace diff
             return value + scalar_;
         }
 
-        Output applyToPartial( const Input& value ) const
+        Output applyToPartial( double, const Input& partial ) const
         {
-            return value;
+            return partial;
         }
 
     private:
@@ -102,13 +104,13 @@ namespace diff
     };
 
     template< typename Expr >
-    ScalarUnaryOp< Expr, UnaryAdditionOp< Expr > > operator+( const Expr& scalar, double additee )
+    ScalarUnaryOp< Expr, UnaryAdditionOp< Expr > > operator+( const ScalarBase< Expr >& scalar, double additee )
     {
-        return ScalarUnaryOp< Expr, UnaryAdditionOp< Expr > >( scalar, UnaryAdditionOp< Expr >( additee ) );
+        return ScalarUnaryOp< Expr, UnaryAdditionOp< Expr > >( static_cast< const Expr& >( scalar ), UnaryAdditionOp< Expr >( additee ) );
     }
 
     template< typename Expr >
-    ScalarUnaryOp< Expr, UnaryAdditionOp< Expr > > operator+( double additee, const Expr& scalar )
+    ScalarUnaryOp< Expr, UnaryAdditionOp< Expr > > operator+( double additee, const ScalarBase< Expr >& scalar )
     {
         return scalar + additee;
     }
@@ -132,9 +134,9 @@ namespace diff
             return value * scalar_;
         }
 
-        Output applyToPartial( const Input& value ) const
+        Output applyToPartial( double, const Input& partial ) const
         {
-            return value * scalar_;
+            return partial * scalar_;
         }
 
     private:
@@ -143,18 +145,40 @@ namespace diff
 
 
     template< typename Expr >
-    ScalarUnaryOp< Expr, UnaryMultiplyOp< Expr > > operator*( const Expr& scalar, double multiplier )
+    ScalarUnaryOp< Expr, UnaryMultiplyOp< Expr > > operator*( const ScalarBase< Expr >& scalar, double multiplier )
     {
-        return ScalarUnaryOp< Expr, UnaryMultiplyOp< Expr > >( scalar, UnaryMultiplyOp< Expr >( multiplier ) );
+        return ScalarUnaryOp< Expr, UnaryMultiplyOp< Expr > >( static_cast< const Expr& >( scalar ), UnaryMultiplyOp< Expr >( multiplier ) );
     }
 
     template< typename Expr >
-    ScalarUnaryOp< Expr, UnaryMultiplyOp< Expr > > operator*( double multiplier, const Expr& scalar )
+    ScalarUnaryOp< Expr, UnaryMultiplyOp< Expr > > operator*( double multiplier, const ScalarBase< Expr >& scalar )
     {
         return scalar * multiplier;
     }
 
-    
+
+    template< typename Expr >
+    struct SineOp
+    {
+        using Input = typename Expr::Partial;
+        using Output = decltype( std::declval< Input >().operator*( double() ) );
+
+        double applyToValue( double value ) const
+        {
+            return sin( value );
+        }
+
+        Output applyToPartial( double value, const Input& partial ) const
+        {
+            return partial * cos( value );
+        }
+    };
+
+    template< typename Expr >
+    ScalarUnaryOp< Expr, SineOp< Expr > > sin( const ScalarBase< Expr >& scalar )
+    {
+        return ScalarUnaryOp< Expr, SineOp< Expr > >( static_cast< const Expr& >( scalar ), SineOp< Expr >() );
+    }
 
 } // diff
 
