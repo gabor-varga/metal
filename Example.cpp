bdc666e02ee11fdba5e7be68c269eecdefe54602
Gabor Varga <gabor@agora.hu>
2019-02-26 22:49:18 +0100

    Misc change to example.

--- Example.cpp
+++ Example.cpp
@@ -7,12 +7,14 @@ int main()
     std::cout << x << std::endl;
 
     // auto y = 4.0 * ( x * 3.0 + 1.5 );
-    auto y = 4.0 * ( sin( x ) * 3.0 + 1.5 );
+    const auto y = 4.0 * ( sin( x ) * 3.0 + 1.5 );
     // auto y = sin( x );
     // auto y = 4.0 * sin( x );
     // auto y = x * 3.0;
     // auto y = x * 2.0;
-    // std::cout << y.at( x.parameters().front() ) << std::endl;
+
+    // const auto p = x.parameters().front();
+    // std::cout << y.at( p ) << std::endl;
     
     metal::Scalar z = y;
     std::cout << z << std::endl;
