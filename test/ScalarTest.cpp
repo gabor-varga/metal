4d909ff9002e0fa7fef7b9453cd5aecba01475af
Gabor Varga <gabor@agora.hu>
2019-03-23 15:50:18 +0100

    Added test for second constructor.

--- test/ScalarTest.cpp
+++ test/ScalarTest.cpp
@@ -7,21 +7,30 @@ using namespace metal;
 
 TEST_CASE( "Scalars can be created", "[scalar_construct]" )
 {
-    Scalar x{ 2.5 };
-
-    REQUIRE( x.value() == 2.5 );
-    REQUIRE( x.partial() == Scalar::Partial{} );
-    REQUIRE( x.parameterMap() == ParameterMap{} );
-    REQUIRE( x.dim() == 0 );
-    REQUIRE( x.size() == 0 );
-    REQUIRE( x.parameters() == ParameterPtrVector{} );
-    REQUIRE_THROWS( x.at( nullptr ) );
-
-    SECTION( "resizing bigger changes size and capacity" )
+    SECTION( "Simple construction from value" )
     {
-        // x.resize( 10 );
+        Scalar x{ 2.5 };
 
-        // REQUIRE( v.size() == 10 );
-        // REQUIRE( v.capacity() >= 10 );
+        REQUIRE( x.value() == 2.5 );
+        REQUIRE( x.partial() == Scalar::Partial{} );
+        REQUIRE( x.parameterMap() == ParameterMap{} );
+        REQUIRE( x.dim() == 0 );
+        REQUIRE( x.size() == 0 );
+        REQUIRE( x.parameters() == ParameterPtrVector{} );
+        REQUIRE_THROWS( x.at( nullptr ) );
+    }
+
+    SECTION( "Construction from value by giving a name" )
+    {
+        Scalar x{ 3.5, "x" };
+        const auto p = x.parameters().front();
+
+        REQUIRE( x.value() == 3.5 );
+        REQUIRE( x.partial() == Scalar::Partial::Ones( 1 ) );
+        REQUIRE( x.parameterMap() == ParameterMap{ { p, 0 } } );
+        REQUIRE( x.dim() == 1 );
+        REQUIRE( x.size() == 1 );
+        REQUIRE( x.parameters() == ParameterPtrVector{ p } );
+        REQUIRE( x.at( p ) == Scalar::Partial::Ones( 1 ) );
     }
 }
