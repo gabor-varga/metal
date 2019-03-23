0792947bf747f50a347b1e161e6d5f1c9ebbeedd
Gabor Varga <gabor@agora.hu>
2019-03-23 15:36:36 +0100

    Basic test.

--- test/ScalarTest.cpp
+++ test/ScalarTest.cpp
@@ -1,54 +1,27 @@
+#include "../include/Scalar.h"
 #include "catch.hpp"
 
-SCENARIO( "vectors can be sized and resized", "[vector]" )
+
+using namespace metal;
+
+
+TEST_CASE( "Scalars can be created", "[scalar_construct]" )
 {
+    Scalar x{ 2.5 };
 
-    GIVEN( "A vector with some items" )
+    REQUIRE( x.value() == 2.5 );
+    REQUIRE( x.partial() == Scalar::Partial{} );
+    REQUIRE( x.parameterMap() == ParameterMap{} );
+    REQUIRE( x.dim() == 0 );
+    REQUIRE( x.size() == 0 );
+    REQUIRE( x.parameters() == ParameterPtrVector{} );
+    REQUIRE_THROWS( x.at( nullptr ) );
+
+    SECTION( "resizing bigger changes size and capacity" )
     {
-        std::vector< int > v( 5 );
+        // x.resize( 10 );
 
-        REQUIRE( v.size() == 5 );
-        REQUIRE( v.capacity() >= 5 );
-
-        WHEN( "the size is increased" )
-        {
-            v.resize( 10 );
-
-            THEN( "the size and capacity change" )
-            {
-                REQUIRE( v.size() == 10 );
-                REQUIRE( v.capacity() >= 10 );
-            }
-        }
-        WHEN( "the size is reduced" )
-        {
-            v.resize( 0 );
-
-            THEN( "the size changes but not capacity" )
-            {
-                REQUIRE( v.size() == 0 );
-                REQUIRE( v.capacity() >= 5 );
-            }
-        }
-        WHEN( "more capacity is reserved" )
-        {
-            v.reserve( 10 );
-
-            THEN( "the capacity changes but not the size" )
-            {
-                REQUIRE( v.size() == 5 );
-                REQUIRE( v.capacity() >= 10 );
-            }
-        }
-        WHEN( "less capacity is reserved" )
-        {
-            v.reserve( 0 );
-
-            THEN( "neither size nor capacity are changed" )
-            {
-                REQUIRE( v.size() == 5 );
-                REQUIRE( v.capacity() >= 5 );
-            }
-        }
+        // REQUIRE( v.size() == 10 );
+        // REQUIRE( v.capacity() >= 10 );
     }
 }
